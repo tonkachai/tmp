ddb6a254518af00489dca45ac38dc7ff8c3f7339
Tontrakan Kachai <tontrakan.k@kbtg.tech>
2025-08-27 11:34:17 +0700

    feat: init Vite + React 19 project with Hello World

---
+++ README.md
@@ -0,0 +1,8 @@
+# Vite + React Hello
+
+This project was initialized to provide a minimal Vite + React "Hello World" app.
+
+How to run:
+
+1. npm install
+2. npm run dev
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Hello World</h1>
+      <p>This is a Vite + React starter created per your request.</p>
+    </div>
+  )
+}
